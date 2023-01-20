d5ac68cd402e2dc458d1247d066d95b44d7d5cee
akashmungra <akashmungra1@gmail.com>
2023-01-19 19:21:32 -0800

    first commit

---
+++ website/JS/main.js
@@ -0,0 +1,24 @@
+/*
+document.getElementById('button').onclick = function(){
+    document.getElementById('out').innerHTML = "123";
+
+}
+
+*/
+document.getElementById('button').addEventListener("click", display);
+
+function display(){
+    let form = document.getElementById('f').elements;
+    document.getElementById('out').innerHTML = "BUTTON CLICKED";
+}
+
+
+let myButton = document.getElementById('button');
+myButton.style.height = '100px';
+myButton.style.width= '100px';
+function formHandle(){
+    let form = document.getElementById('f').elements;
+    for(let i = 1; i < 6; ++i){
+        sessionStorage.setItem("item"+i, form[i-1].value);
+    }
+}

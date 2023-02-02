9b3bd332b9b9c5c28dbcab9fb630709eee7fa906
akashmungra <akashmungra1@gmail.com>
2023-02-01 17:21:12 -0800

    stage 1.5

--- website/midware/main.js
+++ website/midware/main.js
@@ -4,15 +4,12 @@ let hList;
 //recieves output data from socket and displays it on home.html
 //stores output/input data for history.html
 socket.on("gtp", (text)=>{
-    console.log("recieved text");
-
     hList = JSON.parse(sessionStorage.getItem('history'));
     hList.push({
         i: sessionStorage.getItem('i'),
         c: sessionStorage.getItem('c'),
         o: text
     });
-    console.log(hList);
     sessionStorage.setItem('history', JSON.stringify(hList));
     console.log(sessionStorage.getItem('history'));
     document.getElementById("output").innerHTML = text;
@@ -44,7 +41,6 @@ function callTabs(){
 
 //send input information through the socket
 function debug(){
-    console.log("buttonClicked")
     let instructions = document.getElementById('instructions');
     let codeblock = document.getElementById('codeblock');
     if(instructions.value == "" || codeblock.value == ""){
@@ -62,7 +58,6 @@ function debug(){
 //pull history array from sessionStorage and display for history.html
 function addPrompts(){
     hList = JSON.parse(sessionStorage.getItem('history'));
-    console.log(hList);
     hList.forEach(e => {
         let curI = document.createTextNode("Instructions: " + e.i);
         let curC = document.createTextNode("Code: " + e.c);
@@ -77,20 +72,12 @@ function addPrompts(){
         let prompts = [pI, pC, pO];
         let c = -1;
         prompts.forEach(p => {
-            console.log("Loop #", c+2);
             p.className = "p" + (++c+1);
-            console.log(p === prompts[c+1]);
-            console.log("p.className:", p.className);
             p.style = "white-space: pre-line";
             p.appendChild(curs[c]);
             document.getElementById("promptList").appendChild(p);
         })
         
-        /*
-        document.getElementById("promptList").appendChild(pI);
-        document.getElementById("promptList").appendChild(pC);
-        document.getElementById("promptList").appendChild(pO);
-        */
         document.getElementById("promptList").appendChild(document.createElement("br"))
     
     })

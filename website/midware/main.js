48d0b1ba5ec5cbfe8b7faa106a2746618948645e
unknown <fakeplasticcrab@gmail.com>
2023-02-01 20:56:14 -0500

    stage -5

--- website/midware/main.js
+++ website/midware/main.js
@@ -23,29 +23,63 @@ function handleTabs(e){
     }
 }
 
-document.getElementById('codeblock').addEventListener('keydown', handleTabs);
-document.getElementById('instructions').addEventListener('keydown', handleTabs);
+    document.getElementById('codeblock').addEventListener('keydown', handleTabs);
+    document.getElementById('instructions').addEventListener('keydown', handleTabs);
 
 function debug(){
-    console.log("buttonClicked")
     let instructions = document.getElementById('instructions');
     let codeblock = document.getElementById('codeblock');
     if(instructions.value == "" || codeblock.value == ""){
         alert("Please enter text in both boxes");
         return;
     }
+    sessionStorage.setItem('i', instructions.value);
+    sessionStorage.setItem('c', codeblock.value);
+    
     socket.emit("m", {i: instructions.value, c: codeblock.value});
     document.getElementById("output").innerHTML = "Generating response..."
-    document.getElementById('result').innerHTML = instructions.value + " " + codeblock.value;
-    sessionStorage.setItem("instructions", instructions.value);
-    sessionStorage.setItem("code", codeblock.value);
 }
 
-socket.on("output", (m)=>{
-    if(m <= 2){
-        socket.emit("alert", ++m);
-        console.log("from server:", "asdf");
+
+//pull history array from sessionStorage and display for history.html
+function addPrompts(){
+    hList = JSON.parse(sessionStorage.getItem('history'));
+    hList.forEach(e => {
+        let curI = document.createTextNode("Instructions: " + e.i);
+        let curC = document.createTextNode("Code: " + e.c);
+        let curO = document.createTextNode("Output: " + e.o);
+        let curs = [curI, curC, curO];
+
+        let pI = document.createElement("p");
+        let pC = document.createElement("p");
+        let pO = document.createElement("p");
+
+        
+        let prompts = [pI, pC, pO];
+        let c = -1;
+        prompts.forEach(p => {
+            p.className = "p" + (++c+1);
+            p.style = "white-space: pre-line";
+            p.appendChild(curs[c]);
+            document.getElementById("promptList").appendChild(p);
+        })
+        document.getElementById("promptList").appendChild(document.createElement("br"));
+        let outputs = document.getElementsByClassName("p3");
+        for(let i = 0; i < outputs.length; i++){
+            outputs[i].addEventListener('click', copyText);
+        }
+        console.log(outputs)
+    })
+}
+
+function copyText(){ 
+    if(this.innerHTML == ""){
+        return;
     }
-});
+    navigator.clipboard.writeText(this.innerHTML);
+    alert("Output copied to clipboard.")
+}
 
-
+function copyOutput(){
+    document.getElementById("output").addEventListener('click', copyText);
+}
